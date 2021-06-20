4ac5953b1d728ddebfbc016bc0ba86b10f29c996
yangl <156451726@qq.com>
2021-06-20 14:22:42 +0800

    create src/ build/ main.cpp CMakeLists.txt

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: 2021年06月20日 星期日 14时12分55秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
